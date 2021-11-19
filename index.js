eb57146800a32df49cacff7ff796e359c3da9b45
tilak-13 <67039398+tilak-13@users.noreply.github.com>
2021-11-19 22:07:48 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function toggle1(){
+    var content = document.getElementById('hidden-1');
+
+    var display = content.style.display;
+
+    var ques = document.getElementById('q-1');
+
+    if (display == 'block') {
+      content.style.display = 'none';
+    }
+    else {
+      content.style.display = 'block';
+    }
+  };
+function toggle2(){
+    var content = document.getElementById('hidden-2');
+
+    var display = content.style.display;
+
+    var ques = document.getElementById('q-2');
+
+    if (display == 'block') {
+      content.style.display = 'none';
+    }
+    else {
+      content.style.display = 'block';
+    }
+  };
